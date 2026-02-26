127560bd77b2c218c827694f72d39214aefbe611
yashwanth.g@vtiger.com <yashwanth.g@vtiger.com>
2026-02-26 17:39:21 +0530

    test commit

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Hero from "./components/Hero/Hero";
+import HomePage from "./pages/HomePage/HomePage";
+import Navbar from "./components/Navbar/Navbar";
+import { StyledEngineProvider } from "@mui/material/styles";
+import { Outlet } from "react-router-dom";
+import {
+  fetchFilters,
+  fetchNewAlbums,
+  fetchSongs,
+  fetchTopAlbums,
+} from "./api/api";
 
 function App() {
+  const [data, setData] = useState({});
+
+  // const r = {
+  //   topAlbums: [{}, {}, {}, {}],
+  //    newAlbums: [{}, {}, {}, {}],
+  //    genres: ['rock', 'pop', 'jazz'],
+  //    songs: []
+  // };
+
+  const generateData = (key, source) => {
+    source().then((data) => {
+      setData((prevState) => {
+        // Object.assign would also work
+        return { ...prevState, [key]: data };
+      });
+    });
+  };
+
+  useEffect(() => {
+    generateData("topAlbums", fetchTopAlbums);
+    generateData("newAlbums", fetchNewAlbums);
+    generateData("songs", fetchSongs);
+    generateData("genres", fetchFilters);
+  }, []);
+
+  const { topAlbums = [], newAlbums = [], songs = [], genres = [] } = data;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <StyledEngineProvider injectFirst>
+        <Navbar searchData={[...topAlbums, ...newAlbums]} />
+        <Outlet context={{ data: { topAlbums, newAlbums, songs, genres } }} />
+      </StyledEngineProvider>
+    </>
   );
 }
+
+// {data: {
+//   topAlbums: [],
+//   newAlbums: [],
+//   genres: [],
+//   songs: []
+// }}
 
 export default App;
---
+++ qtify/src/components/Carousel/Carousel.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import styles from "./Carousel.module.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import CarouselLeftNavigation from "./CarouselLeftNavigation/CarouselLeftNavigation";
+import CarouselRightNavigation from "./CarouselRightNavigation/CarouselRightNavigation";
+
+const Controls = ({ data }) => {
+  const swiper = useSwiper();
+
+  useEffect(() => {
+    swiper.slideTo(0);
+  }, [data]);
+
+  return <></>;
+};
+
+function Carousel({ data, renderComponent }) {
+  return (
+    <div className={styles.wrapper}>
+      <Swiper
+        style={{ padding: "0px 20px" }}
+        initialSlide={0}
+        modules={[Navigation]}
+        slidesPerView={"auto"}
+        spaceBetween={40}
+        allowTouchMove
+      >
+        <Controls data={data} />
+        <div>
+          <CarouselLeftNavigation />
+          <CarouselRightNavigation />
+        </div>
+        {data.map((ele) => (
+          <SwiperSlide>{renderComponent(ele)}</SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
+
+export default Carousel;
